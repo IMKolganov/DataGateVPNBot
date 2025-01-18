39b103eef1fccfca3dc26ba6b0bf1c8da7d2ff97
Ivan Kolganov <imkolganov@gmail.com>
2025-01-18 23:06:18 +0200

    added system command for registration all commands in chat

---
+++ Services/TelegramSettingsService.cs
@@ -0,0 +1,8 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace DataGateVPNBotV1.Services;
+
+public class TelegramSettingsService
+{
+}
--- Services/UpdateHandler.cs
+++ Services/UpdateHandler.cs
@@ -119,6 +119,8 @@ public class UpdateHandler : IUpdateHandler
             "/about_project" => AboutProject(msg),
             "/contacts" => Contacts(msg),
             "/change_language" => SelectLanguage(msg),
+            
+            "/register_commands" => await RegisterCommandsAsync(msg),
 
             "/photo" => SendPhoto(msg),
             "/inline_buttons" => SendInlineKeyboard(msg),
@@ -532,5 +534,63 @@ public class UpdateHandler : IUpdateHandler
     {
         _logger.LogInformation("Unknown update type: {UpdateType}", update.Type);
         return Task.CompletedTask;
+    }
+    
+    private async Task<Message> RegisterCommandsAsync(Message msg)
+    {
+        var commandsEn = new[]
+        {
+            new BotCommand { Command = "register", Description = "Register to use the VPN" },
+            new BotCommand { Command = "get_my_files", Description = "Get your files for connecting to the VPN" },
+            new BotCommand { Command = "make_new_file", Description = "Create a new file for connecting to the VPN" },
+            new BotCommand { Command = "delete_selected_file", Description = "Delete a specific file" },
+            new BotCommand { Command = "delete_all_files", Description = "Delete all files" },
+            new BotCommand { Command = "how_to_use", Description = "Instructions on how to use the VPN" },
+            new BotCommand { Command = "install_client", Description = "Get a link to download OpenVPN client" },
+            new BotCommand { Command = "about_bot", Description = "Information about the bot" },
+            new BotCommand { Command = "about_project", Description = "Information about the project" },
+            new BotCommand { Command = "contacts", Description = "Developer contacts" },
+            new BotCommand { Command = "change_language", Description = "Change your language" },
+        };
+
+        var commandsRu = new[]
+        {
+            new BotCommand { Command = "register", Description = "Зарегистрируйтесь для использования VPN" },
+            new BotCommand { Command = "get_my_files", Description = "Получите свои файлы для подключения к VPN" },
+            new BotCommand { Command = "make_new_file", Description = "Создайте новый файл для подключения к VPN" },
+            new BotCommand { Command = "delete_selected_file", Description = "Удалить выбранный файл" },
+            new BotCommand { Command = "delete_all_files", Description = "Удалить все файлы" },
+            new BotCommand { Command = "how_to_use", Description = "Инструкция по использованию VPN" },
+            new BotCommand { Command = "install_client", Description = "Ссылка на загрузку клиента OpenVPN" },
+            new BotCommand { Command = "about_bot", Description = "Информация о боте" },
+            new BotCommand { Command = "about_project", Description = "Информация о проекте" },
+            new BotCommand { Command = "contacts", Description = "Контакты разработчика" },
+            new BotCommand { Command = "change_language", Description = "Изменить язык" },
+        };
+
+        var commandsEl = new[]
+        {
+            new BotCommand { Command = "register", Description = "Εγγραφείτε για να χρησιμοποιήσετε το VPN" },
+            new BotCommand { Command = "get_my_files", Description = "Αποκτήστε τα αρχεία σας για σύνδεση στο VPN" },
+            new BotCommand { Command = "make_new_file", Description = "Δημιουργήστε ένα νέο αρχείο για σύνδεση στο VPN" },
+            new BotCommand { Command = "delete_selected_file", Description = "Διαγραφή συγκεκριμένου αρχείου" },
+            new BotCommand { Command = "delete_all_files", Description = "Διαγραφή όλων των αρχείων" },
+            new BotCommand { Command = "how_to_use", Description = "Οδηγίες χρήσης VPN" },
+            new BotCommand { Command = "install_client", Description = "Λήψη του OpenVPN client" },
+            new BotCommand { Command = "about_bot", Description = "Πληροφορίες για το bot" },
+            new BotCommand { Command = "about_project", Description = "Πληροφορίες για το έργο" },
+            new BotCommand { Command = "contacts", Description = "Στοιχεία επικοινωνίας του προγραμματιστή" },
+            new BotCommand { Command = "change_language", Description = "Αλλάξτε τη γλώσσα σας" },
+        };
+
+        await _botClient.SetMyCommands(commandsEn, languageCode: "en");
+        await _botClient.SetMyCommands(commandsRu, languageCode: "ru");
+        await _botClient.SetMyCommands(commandsEl, languageCode: "el");
+        
+        return await _botClient.SendMessage(
+            chatId: msg.Chat.Id,
+            text: "\u2705 All commands have been successfully registered...",
+            replyMarkup: new ReplyKeyboardRemove()
+        );
     }
 }
