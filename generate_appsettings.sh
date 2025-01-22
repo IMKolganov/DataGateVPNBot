99910c19ec6b89928f101c3cab20ff74acbae7d4
Ivan Kolganov <imkolganov@gmail.com>
2025-01-22 12:58:36 +0200

    upd appsettings

---
+++ generate_appsettings.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+envsubst < appsettings.json.template > appsettings.json
+echo "Файл appsettings.json completed."
+
+envsubst < appsettings.json.template > appsettings.Development.json
+echo "Файл appsettings.Development.json completed."
+
+git add appsettings.json.template
