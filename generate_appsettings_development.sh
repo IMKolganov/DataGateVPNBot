f8d14bacb4898c5ae38f1a5be72ce0b1aa05f55b
Ivan Kolganov <imkolganov@gmail.com>
2025-01-23 16:25:23 +0200

    added generate_appsettings_development

---
+++ generate_appsettings_development.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+if [ -f .env ]; then
+  export $(grep -v '^#' .env_dev | xargs)
+fi
+
+envsubst < appsettings.json.template > appsettings.Development.json
+echo "File appsettings.Development.json is completed."
+
+git add appsettings.json.template
