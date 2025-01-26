bef4738f7b641861e1a765dacf7cb3e3704e063a
Ivan Kolganov <imkolganov@gmail.com>
2025-01-26 09:11:35 +0200

    added defaults value

--- Models/Helpers/ElasticsearchSettings.cs
+++ Models/Helpers/ElasticsearchSettings.cs
@@ -3,7 +3,7 @@
 public class ElasticsearchSettings
 {
     public string Uri { get; set; } = "http://localhost:9200";
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public string IndexFormat { get; set; } = "app-logs-{0:yyyy.MM.dd}";
 }
